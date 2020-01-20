7f3c3c0b83f34d5b2ea2a667c967a833a75a3dde
Alexander Lyutikov <lyutiko.alex@gmail.com>
2020-01-20 03:07:53 +0300

    Initial commit

---
+++ train.py
@@ -0,0 +1,110 @@
+import os
+import shutil
+from tqdm import tqdm
+import torch
+import numpy as np
+import torchvision
+import matplotlib.pyplot as plt
+import time
+import copy
+from torchvision import transforms, models
+
+train_dir = 'train'
+val_dir = 'val'
+data_root = '/input'
+
+class_names = ['cleaned', 'dirty']
+
+for dir_name in [train_dir, val_dir]:
+    for class_name in class_names:
+        os.makedirs(os.path.join(dir_name, class_name), exist_ok=True)
+
+for class_name in class_names:
+    source_dir = os.path.join(data_root, 'train', class_name)
+    for i, file_name in enumerate(tqdm(os.listdir(source_dir))):
+        if i % 6 != 0:
+            dest_dir = os.path.join(train_dir, class_name)
+        else:
+            dest_dir = os.path.join(val_dir, class_name)
+        shutil.copy(os.path.join(source_dir, file_name), os.path.join(dest_dir, file_name))
+
+train_transforms = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+val_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+train_dataset = torchvision.datasets.ImageFolder(train_dir, train_transforms)
+val_dataset = torchvision.datasets.ImageFolder(val_dir, val_transforms)
+
+batch_size = 8
+train_dataloader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True, num_workers=batch_size)
+val_dataloader = torch.utils.data.DataLoader(
+    val_dataset, batch_size=batch_size, shuffle=False, num_workers=batch_size)
+
+X_batch, y_batch = next(iter(train_dataloader))
+mean = np.array([0.485, 0.456, 0.406])
+std = np.array([0.229, 0.224, 0.225])
+plt.imshow(X_batch[0].permute(1, 2, 0).numpy() * std + mean);
+
+
+def train_model(model, loss, optimizer, scheduler, num_epochs):
+    device = torch.device('cuda:0')
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}:'.format(epoch, num_epochs - 1), flush=True)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                dataloader = train_dataloader
+                scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                dataloader = val_dataloader
+                model.eval()  # Set model to evaluate mode
+
+            running_loss = 0.
+            running_acc = 0.
+
+            # Iterate over data.
+            for inputs, labels in tqdm(dataloader):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+
+                # forward and backward
+                with torch.set_grad_enabled(phase == 'train'):
+                    preds = model(inputs)
+                    loss_value = loss(preds, labels)
+                    preds_class = preds.argmax(dim=1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss_value.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss_value.item()
+                running_acc += (preds_class == labels.data).float().mean()
+
+            epoch_loss = running_loss / len(dataloader)
+            epoch_acc = running_acc / len(dataloader)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc), flush=True)
+
+    return model
+
+
+model = models.resnext101_32x8d(pretrained=False)
+model.fc = torch.nn.Hardtanh(model.fc.in_features, )
+
