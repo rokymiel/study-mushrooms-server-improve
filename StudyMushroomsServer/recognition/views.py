14c402d9c9f13a1129ca52abbf106a114ce37b1b
rokymiel <rokymiel@gmail.com>
2022-05-14 00:38:38 +0300

    Доработано вью распознавания грибов

--- StudyMushroomsServer/recognition/views.py
+++ StudyMushroomsServer/recognition/views.py
@@ -1,12 +1,12 @@
 from django.core.files.base import ContentFile
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 from StudyMushroomsServer.logger import base_logger
 from common.constants import classnames
-from recognition.validate import mushrooms_recognition_model
+from recognition.validate import mushrooms_recognition_model, current_device, predict_probs, recognize
 from .serializers import *
 from PIL import Image
 import torch
@@ -20,32 +20,19 @@ logger = base_logger.getChild('recognition')
 class RecognizeView(ListModelMixin, GenericAPIView):
     serializer_class = RecognizeSerializer
     pagination_class = LimitOffsetPagination
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
         file = ContentFile(base64.b64decode(request.data.get('image')))
         image = Image.open(file.open())
-        preprocess = torchvision.transforms.Compose([
-            torchvision.transforms.Resize(256),
-            torchvision.transforms.CenterCrop(224),
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
-        tensor = preprocess(image)
-        batch = tensor.unsqueeze(0)
 
-        if torch.cuda.is_available():
-            batch = batch.to('cuda')
-            mushrooms_recognition_model.to('cuda')
-        with torch.no_grad():
-            output = mushrooms_recognition_model(batch)
-
-        probs = torch.nn.functional.softmax(output[0], dim=0)
-        res = []
         mushrooms = Mushroom.objects.all()
-        for i in range(len(classnames)):
-            if probs[i].item().__float__() > 0.01:
-                res.append(RecognizeModel(probability=probs[i].item().__float__(),
-                                          mushroom=mushrooms.filter(classname=classnames[i])[0]))
+        def create_model(probability, classname):
+            return RecognizeModel(probability=probability,
+                                  mushroom=mushrooms.filter(classname=classname)[0])
+
+        res = recognize(image, create_model)
+
         res = sorted(res, key=lambda x: -x.probability)
         ser = self.get_serializer(res, many=True)
         print(ser)
--- recognition/validate.py
+++ recognition/validate.py
@@ -1,10 +1,40 @@
 import torch
 import torchvision
 
+import common.constants
 from common.constants import classnames
 
 mushrooms_recognition_model = torchvision.models.resnet50()
 mushrooms_recognition_model.fc = torch.nn.Linear(mushrooms_recognition_model.fc.in_features, len(classnames))
-device = torch.device('cpu')
-mushrooms_recognition_model.load_state_dict(torch.load('nn', map_location=device))
+current_device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+mushrooms_recognition_model.load_state_dict(torch.load('nn', map_location=current_device))
 mushrooms_recognition_model.eval()
+
+def predict_probs(image):
+    preprocess = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(256),
+        torchvision.transforms.CenterCrop(224),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    tensor = preprocess(image)
+    batch = tensor.unsqueeze(0)
+
+    batch = batch.to(current_device)
+    mushrooms_recognition_model.to(current_device)
+    with torch.no_grad():
+        output = mushrooms_recognition_model(batch)
+
+    return torch.nn.functional.softmax(output[0], dim=0)
+
+def recognize(image, model_creator, threshold=0.01):
+    probs = predict_probs(image)
+    res = []
+
+    for prob, classname in zip(probs, classnames):
+        probability = prob.item().__float__()
+        if probability > threshold:
+            r_model = model_creator(probability, classname)
+            res.append(r_model)
+
+    return res
